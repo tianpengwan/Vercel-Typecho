d03a32f0072069c0b7d7fddfa797f28a65ca70c7
tianpengwan <102905945+tianpengwan@users.noreply.github.com>
2024-07-08 15:41:22 +0800

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -8,19 +8,19 @@
  */
  
 /** 开启https */ 
-define('__TYPECHO_SECURE__',true);
+define('__TYPECHO_SECURE__'，true);
 
 /** 定义根目录 */
-define('__TYPECHO_ROOT_DIR__', dirname(__FILE__));
+define('__TYPECHO_ROOT_DIR__'， dirname(__FILE__));
 
 /** 定义插件目录(相对路径) */
-define('__TYPECHO_PLUGIN_DIR__', '/usr/plugins');
+define('__TYPECHO_PLUGIN_DIR__'， '/usr/plugins');
 
 /** 定义模板目录(相对路径) */
-define('__TYPECHO_THEME_DIR__', '/usr/themes');
+define('__TYPECHO_THEME_DIR__'， '/usr/themes');
 
 /** 后台路径(相对路径) */
-define('__TYPECHO_ADMIN_DIR__', '/admin/');
+define('__TYPECHO_ADMIN_DIR__'， '/admin/');
 
 /** 设置包含路径 */
 @set_include_path(get_include_path() . PATH_SEPARATOR .
@@ -36,12 +36,12 @@ Typecho_Common::init();
 /** 定义数据库参数 */
 $db = new Typecho_Db('Pdo_Mysql', 'typecho_');
 $db->addServer(array (
-  'host' => '数据库地址',
-  'user' => '数据库用户',
-  'password' => '数据库密码',
+  'host' => 'sql.freedb.tech',
+  'user' => 'freedb_tianpeng',
+  'password' => '9@jwHUNMS7Tu*NF',
   'charset' => 'utf8mb4',
   'port' => '3306',
-  'database' => '数据库名',
+  'database' => 'freedb_tianpeng',
   'engine' => 'MyISAM',
 ), Typecho_Db::READ | Typecho_Db::WRITE);
 Typecho_Db::set($db);
